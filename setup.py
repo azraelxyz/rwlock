c7d90d95f4e0f2cb63a5f0510b907a728993280e
xwhuang <xwhuang@qnap.com>
2015-10-21 14:12:09 +0800

    rename README.md to README

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     name="rwlock",
     packages=find_packages(),
 
-    version="0.0.1",
+    version="0.0.3",
     author="kristjan.jonsson",
     maintainer="xwhuang",
     url="https://bugs.python.org/issue8800",
@@ -17,6 +17,6 @@ setup(
     description=("A read-write lock for python,"
                  "it is from "
                  "https://bugs.python.org/issue8800 and patched for py2.7"),
-    long_description=read('README.md'),
+    long_description=read('README'),
     keywords=['rwlock', 'read-write lock', 'lock']
 )

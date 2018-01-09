20d0a558e538abeb98dc46fbe320730d9fd4e6d4
xwhuang <xwhuang@qnap.com>
2018-01-09 15:41:13 +0800

    add kristjan.jonsson as maintainer in setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     name="rwlock",
     packages=find_packages(),
 
-    version="0.0.6",
+    version="0.0.7",
     author="kristjan.jonsson",
-    maintainer="xwhuang",
+    maintainer="kristjan.jonsson xwhuang",
     url="https://bugs.python.org/issue8800",
     download_url="https://github.com/azraelxyz/rwlock",
     description=("A read-write lock for python,"
